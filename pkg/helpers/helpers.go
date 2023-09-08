ebc2c54e09cb0d39959caabf50c1fc7eda7e6441
andreistan26 <andreistan2003@gmail.com>
2023-09-08 14:35:58 +0300

    Add helpers to cover for go's minimal standard library

---
+++ pkg/helpers/helpers.go
@@ -0,0 +1,31 @@
+package helpers
+
+// Convert from string to null terminated byte slice
+func String2Bytes(str string) []byte {
+	bytes := []byte(str)
+	bytes = append(bytes, '\x00')
+
+	return bytes
+}
+
+// Find item in slice and return it's index, if none found return -1
+func Find[T comparable](needle T, haystack []T) int {
+	for i, v := range haystack {
+		if v == needle {
+			return i
+		}
+	}
+
+	return -1
+}
+
+// Get the first string from a byte stream
+func GetString(bytes []byte) string {
+	for i, v := range bytes {
+		if v == '\x00' {
+			return string(bytes[:i])
+		}
+	}
+
+	return ""
+}
